75696da319292dc89e03eb037cbdd8475986e3c9
SebastianVedrina <58113056+SebastianVedrina@users.noreply.github.com>
2023-04-05 08:59:18 +0200

    cta background

--- frontend-template.php
+++ frontend-template.php
@@ -7,6 +7,7 @@ get_header(); // Load the header
 
 include 'frontend/hero.php'; // Load the hero module
 include 'frontend/color_cards.php';
+include 'frontend/cta_background.php';
 include 'frontend/cta.php';
 
 get_footer(); // Load the footer
