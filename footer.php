f90ec14e5cd1089a0d3e1bc3aefbb92225464683
SebastianVedrina <58113056+SebastianVedrina@users.noreply.github.com>
2023-05-04 11:15:19 +0200

    footer and header mobile

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
                                 <a href="">Contact</a>
                             </li>
                         </ul>
-                        <div class="other-items">
+                        <div class="other-items d-lg-block d-none">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g clip-path="url(#clip0_139_1427)">
                                 <path d="M12 5.9C13.16 5.9 14.1 6.84 14.1 8C14.1 9.16 13.16 10.1 12 10.1C10.84 10.1 9.9 9.16 9.9 8C9.9 6.84 10.84 5.9 12 5.9ZM12 14.9C14.97 14.9 18.1 16.36 18.1 17V18.1H5.9V17C5.9 16.36 9.03 14.9 12 14.9ZM12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4ZM12 13C9.33 13 4 14.34 4 17V19C4 19.55 4.45 20 5 20H19C19.55 20 20 19.55 20 19V17C20 14.34 14.67 13 12 13Z" fill="white"/>
@@ -68,8 +68,21 @@
                             <a href="" class="btn btn-primary">JOIN ARGO</a>
                         </div>
                     </div>
+                    <div class="d-lg-none d-flex justify-content-center align-items-center w-100 btn-wrap">
+                        <a href="" class="btn btn-primary">JOIN ARGO</a>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_139_1427)">
+                            <path d="M12 5.9C13.16 5.9 14.1 6.84 14.1 8C14.1 9.16 13.16 10.1 12 10.1C10.84 10.1 9.9 9.16 9.9 8C9.9 6.84 10.84 5.9 12 5.9ZM12 14.9C14.97 14.9 18.1 16.36 18.1 17V18.1H5.9V17C5.9 16.36 9.03 14.9 12 14.9ZM12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4ZM12 13C9.33 13 4 14.34 4 17V19C4 19.55 4.45 20 5 20H19C19.55 20 20 19.55 20 19V17C20 14.34 14.67 13 12 13Z" fill="white"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_139_1427">
+                            <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
                 </div>
-                <div class="footer-footer d-flex justify-content-between">
+                <div class="footer-footer d-flex justify-content-between align-items-center">
                     <div class="copyright">
                         <span>© 2023 ARGO</span>
                     </div>
