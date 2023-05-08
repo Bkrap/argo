ab19f3a89bf61d51bcf2eb8d9d1378a379ac1ec3
brunoK <brunokrapljan97@gmail.com>
2023-05-08 23:19:53 +0200

    backend + frontend updates

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -7,6 +7,33 @@ function debug( $s ) {
 	echo "</pre>";
 }
 
+function wyswig_raw( $param ) {
+	return strip_tags( $param, "<strong>" );
+}
+
+function get_btn( $button_group, $icon = false ) {
+
+	if( $button_group['show_button'] ) {
+
+		$button = $button_group['button'];
+		include THEME_DIR . '/templates/partials/button.php'; 
+		if( $icon ) { ?>
+			<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g clip-path="url(#clip0_204_1799)">
+				<path d="M7.99984 3.16669L7.05984 4.10669L10.7798 7.83335H2.6665V9.16669H10.7798L7.05984 12.8934L7.99984 13.8334L13.3332 8.50002L7.99984 3.16669Z" fill="#2A807F"/>
+				</g>
+				<defs>
+				<clipPath id="clip0_204_1799">
+				<rect width="16" height="16" fill="white" transform="translate(0 0.5)"/>
+				</clipPath>
+				</defs>
+			</svg>
+		<?php } 
+		
+	}
+
+}
+
 /********************************************************************************* */
 function get_post_by_title($page_title, $post_type ='post' , $output = OBJECT) {
     global $wpdb;
---
+++ templates/acf/info_cards.php
@@ -0,0 +1,34 @@
+<?php //debug($params); ?>
+<section class="info-cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 text-wrapper">
+                <p class="small-title bold-text">
+                    <?php echo $params['pre_title_label']; ?>
+                </p>
+                <h2><?php echo wyswig_raw( $params['title'] ); ?></h2>
+                <p class="big-description"><?php echo wyswig_raw( $params['text'] ); ?></p>
+                <?php get_btn( $params['button_group'], true ); ?>
+            </div>
+            <div class="col-lg-8">
+                <div class="row">
+                    <?php foreach( $params['cards'] as $k => $v ) { ?>
+                        <div class="small-card">
+                            <div class="background d-flex flex-column" style="background: linear-gradient(0deg, rgba(42, 128, 127, 0.8), rgba(42, 128, 127, 0.8)), url('<?php echo wp_get_attachment_image_url( $v['background_image']['id'], 'full' ); ?>'); ; background-position: <?php echo $v['background_image']['left'] . "% " . $v['background_image']['top'] . "%"; ?>">
+                                
+                                <img src="<?php echo $v['icon']['url']; ?>" alt="icon" />
+                                <p class="number">
+                                    0<?php echo $k; ?>
+                                </p>
+                                <div class="text d-flex flex-column justify-content-center">
+                                    <h4 class="title"><?php echo $v['title']; ?></h4>
+                                    <p class="description"><?php echo $v['text']; ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
