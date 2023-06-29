f10b229239feef0a452ea4dd05ec2dd8210c7f80
SebastianVedrina <58113056+SebastianVedrina@users.noreply.github.com>
2023-06-29 16:43:02 +0200

    shop popup modal

--- frontend-template.php
+++ frontend-template.php
@@ -5,25 +5,26 @@
 
 get_header(); // Load the header
 
-include 'frontend/cta.php';
-include 'frontend/cta_slider.php';
-include 'frontend/hero.php';
-include 'frontend/hero3.php';
-include 'frontend/hero2.php';
-include 'frontend/list.php';
-include 'frontend/info_cards_title.php';
-include 'frontend/info_cards.php';
-include 'frontend/news_hub.php';
-include 'frontend/counter.php';
-include 'frontend/facts_and_cards.php';
-include 'frontend/facts_two_columns.php';
-include 'frontend/cta.php';
-include 'frontend/link_cards.php';
+// include 'frontend/cta.php';
+// include 'frontend/cta_slider.php';
+// include 'frontend/hero.php';
+// include 'frontend/hero3.php';
+// include 'frontend/hero2.php';
+// include 'frontend/list.php';
+// include 'frontend/info_cards_title.php';
+// include 'frontend/info_cards.php';
+// include 'frontend/news_hub.php';
+// include 'frontend/counter.php';
+// include 'frontend/facts_and_cards.php';
+// include 'frontend/facts_two_columns.php';
+// include 'frontend/cta.php';
+// include 'frontend/link_cards.php';
+include 'frontend/shop_popup.php';
 
-include 'frontend/news_hub_archive.php';
+// include 'frontend/news_hub_archive.php';
 // include 'frontend/video_hub.php';
 
-include 'frontend/single_post.php';
+// include 'frontend/single_post.php';
 
 
 get_footer(); // Load the footer
---
+++ frontend/shop_popup.php
@@ -0,0 +1,49 @@
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#shopPopup">
+  Launch demo modal
+</button>
+
+
+
+<div class="modal fade" id="shopPopup" tabindex="-1" aria-labelledby="shopPopupLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content" style="background-image: url('http://localhost/argo/wp-content/themes/argo/images/cap.png')">
+    <button type="button" class="close-button" data-bs-dismiss="modal" aria-label="Close">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1523_6168)">
+        <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="white"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_1523_6168">
+        <rect width="24" height="24" fill="white"/>
+        </clipPath>
+        </defs>
+        </svg>
+    </button>
+      <div class="modal-body">
+        <div class="small-wrapper">
+            <div class="pill">
+                <span>NEW</span>
+            </div>
+            <p class="short-description">ARGO MERCH</p>
+        </div>
+        <h2 class="modal-title">Shop <span class="bold-text">the merch!</span></h2>
+        <p>Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+        <a href="https://www.argopath.com/news-hub/" target="" class="btn btn-link_primary">
+		    <span>Latest News</span>
+            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_204_1799)">
+                <path d="M7.99984 3.16669L7.05984 4.10669L10.7798 7.83335H2.6665V9.16669H10.7798L7.05984 12.8934L7.99984 13.8334L13.3332 8.50002L7.99984 3.16669Z" fill="#2A807F"></path>
+                </g>
+                <defs>
+                <clipPath id="clip0_204_1799">
+                <rect width="16" height="16" fill="white" transform="translate(0 0.5)"></rect>
+                </clipPath>
+                </defs>
+            </svg>
+	    </a>
+
+      </div>
+
+    </div>
+  </div>
+</div>
